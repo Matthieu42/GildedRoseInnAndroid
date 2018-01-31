43bc53a66c927fb4048b0f451f920862d796a3f7
Matthieu Tinnes <matthieu.tinnes@iut-valence.fr>
2018-01-31 11:16:06 +0100

    Initial commit

---
+++ app/src/main/java/fr/iut_valence/tinnesm/gildedroseinn/InventoryActivity.java
@@ -0,0 +1,15 @@
+package fr.iut_valence.tinnesm.gildedroseinn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InventoryActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.inventory_layout);
+	}
+}
---
+++ app/src/main/java/fr/iut_valence/tinnesm/gildedroseinn/ShopActivity.java
@@ -0,0 +1,15 @@
+package fr.iut_valence.tinnesm.gildedroseinn package.name.here;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShopActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.shop_layout);
+	}
+}
